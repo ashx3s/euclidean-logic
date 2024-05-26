29cb64c0b482900f38c3bfb478f1e34f1a394cb7
Ashlyn Knox <ashlyn@lilyx.io>
2024-05-26 01:22:44 -0600

    Initial commit Initialize the following files new file:   .gitignore new file:   README.md --> todos and refrences new file:   dist/Point.js new file:   dist/src/Point.js new file:   dist/src/Point.test.js new file:   dist/test/Point.test.js new file:   jest.config.js new file:   package.json new file:   pnpm-lock.yaml new file:   src/Point.test.ts new file:   src/Point.ts new file:   tsconfig.json

---
+++ dist/src/Point.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Point_1 = require("./Point");
+test('create a point on a canvas', () => {
+    console.log(Point_1.Point);
+});
---
+++ src/Point.test.ts
@@ -0,0 +1,5 @@
+import { Point } from "./Point"
+
+test('create a point on a canvas', () => {
+  console.log(Point)
+})
